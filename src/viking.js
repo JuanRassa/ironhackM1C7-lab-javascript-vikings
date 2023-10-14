09e558feb15eafb64ff08bf29b06dd5aeb1f0530
Juan Rassa <jmrassa.dev@gmail.com>
2023-10-14 16:49:47 +0100

    Lab solved with some extra validations and a 'Manual War' result.

--- src/viking.js
+++ src/viking.js
@@ -63,9 +63,10 @@ class War {
     let defenderRandomIndex = Math.floor(Math.random() * defenderArmy.length);
     let defenderSolider = defenderArmy[defenderRandomIndex];
     let result = defenderSolider.receiveDamage(attackerSolider.strength);
-    console.log(defenderSolider.health);
     if (defenderSolider.health <= 0) {
-      console.log(`A ${defenderSolider.constructor.name} has been killed`);
+      console.log(
+        `Attack fatal report! -> A ${defenderSolider.constructor.name} soldier has been killed :(`
+      );
       defenderArmy.splice(defenderRandomIndex, 1);
     }
     // defenderArmy.forEach((soldier, i) => {
@@ -86,32 +87,52 @@ class War {
 
   showStatus() {
     if (this.saxonArmy.length === 0) {
+      console.log('Vikings have won the war of the century!');
       return 'Vikings have won the war of the century!';
     } else if (this.vikingArmy.length === 0) {
+      console.log(
+        'RESULT: Saxons have fought for their lives and survived another day...'
+      );
       return 'Saxons have fought for their lives and survived another day...';
     } else if (this.vikingArmy.length === 1 && this.saxonArmy.length === 1) {
+      console.log(
+        'RESULT: Vikings and Saxons are still in the thick of battle.'
+      );
       return 'Vikings and Saxons are still in the thick of battle.';
     } else {
+      console.log(
+        'RESULT: Vikings and Saxons are still in the thick of battle.'
+      );
       return 'War is still in process...';
     }
   }
 }
 
 //  MANUAL WAR:
+console.log('*** *** MY WAR HAS STARTED *** ***');
 const myWar = new War();
 const s1 = new Viking('1', 80, 50);
 const s2 = new Viking('2', 80, 50);
 const s3 = new Viking('3', 80, 50);
 const s4 = new Viking('4', 100, 50);
-
-const x1 = new Saxon(100, 20);
 myWar.addViking(s1);
 myWar.addViking(s2);
 myWar.addViking(s3);
 myWar.addViking(s4);
 
+const x1 = new Saxon(100, 20);
+const x2 = new Saxon(100, 20);
 myWar.addSaxon(x1);
+myWar.addSaxon(x2);
 
+console.log('Armies BEFORE war:');
+console.log('Viking Army:', myWar.vikingArmy);
+console.log('Saxon Army:', myWar.saxonArmy);
+console.log('******');
+console.log('Battle loses reports begins:');
+myWar.attackArmy(myWar.saxonArmy, myWar.vikingArmy);
+myWar.attackArmy(myWar.saxonArmy, myWar.vikingArmy);
+myWar.attackArmy(myWar.saxonArmy, myWar.vikingArmy);
 myWar.attackArmy(myWar.saxonArmy, myWar.vikingArmy);
 myWar.attackArmy(myWar.saxonArmy, myWar.vikingArmy);
 myWar.attackArmy(myWar.saxonArmy, myWar.vikingArmy);
@@ -128,6 +149,10 @@ myWar.attackArmy(myWar.saxonArmy, myWar.vikingArmy);
 myWar.attackArmy(myWar.vikingArmy, myWar.saxonArmy);
 myWar.attackArmy(myWar.saxonArmy, myWar.vikingArmy);
 myWar.attackArmy(myWar.vikingArmy, myWar.saxonArmy);
-
-console.log(myWar);
-console.log(myWar.showStatus());
+console.log('Battle loses reports ends');
+console.log('******');
+myWar.showStatus();
+console.log('Armies AFTER war:');
+console.log('Viking Army:', myWar.vikingArmy);
+console.log('Saxon Army:', myWar.saxonArmy);
+console.log('*** *** MY WAR HAS ENDED *** ***');
