6a9764b5252c8659fbfa03d895187b6827c91f7f
Juan Rassa <jmrassa.dev@gmail.com>
2023-10-14 16:32:36 +0100

    feat: Add validation, add logs, create a War.
    -Add a validation to check if there are still soldiers in the army before the next attack is made. If not, war is over.
    -Log the health of the defendor in each attack. If 0, it shows a message informing of his death.
    -Create a War.

--- src/viking.js
+++ src/viking.js
@@ -55,13 +55,24 @@ class War {
   }
 
   attackArmy(attackerArmy, defenderArmy) {
+    if (attackerArmy.length === 0 || defenderArmy.length === 0)
+      return 'War is over';
+
     let attackerRandomIndex = Math.floor(Math.random() * attackerArmy.length);
     let attackerSolider = attackerArmy[attackerRandomIndex];
     let defenderRandomIndex = Math.floor(Math.random() * defenderArmy.length);
     let defenderSolider = defenderArmy[defenderRandomIndex];
-
     let result = defenderSolider.receiveDamage(attackerSolider.strength);
-    if (defenderSolider.health <= 0) defenderArmy.splice(defenderArmy, 1);
+    console.log(defenderSolider.health);
+    if (defenderSolider.health <= 0) {
+      console.log(`A ${defenderSolider.constructor.name} has been killed`);
+      defenderArmy.splice(defenderRandomIndex, 1);
+    }
+    // defenderArmy.forEach((soldier, i) => {
+    //   if (soldier.health <= 0) {
+    //     defenderArmy.splice(i, 1);
+    //   }
+    // });
 
     return result;
   }
@@ -80,6 +91,43 @@ class War {
       return 'Saxons have fought for their lives and survived another day...';
     } else if (this.vikingArmy.length === 1 && this.saxonArmy.length === 1) {
       return 'Vikings and Saxons are still in the thick of battle.';
+    } else {
+      return 'War is still in process...';
     }
   }
 }
+
+//  MANUAL WAR:
+const myWar = new War();
+const s1 = new Viking('1', 80, 50);
+const s2 = new Viking('2', 80, 50);
+const s3 = new Viking('3', 80, 50);
+const s4 = new Viking('4', 100, 50);
+
+const x1 = new Saxon(100, 20);
+myWar.addViking(s1);
+myWar.addViking(s2);
+myWar.addViking(s3);
+myWar.addViking(s4);
+
+myWar.addSaxon(x1);
+
+myWar.attackArmy(myWar.saxonArmy, myWar.vikingArmy);
+myWar.attackArmy(myWar.saxonArmy, myWar.vikingArmy);
+myWar.attackArmy(myWar.saxonArmy, myWar.vikingArmy);
+myWar.attackArmy(myWar.saxonArmy, myWar.vikingArmy);
+myWar.attackArmy(myWar.saxonArmy, myWar.vikingArmy);
+myWar.attackArmy(myWar.saxonArmy, myWar.vikingArmy);
+myWar.attackArmy(myWar.saxonArmy, myWar.vikingArmy);
+myWar.attackArmy(myWar.saxonArmy, myWar.vikingArmy);
+myWar.attackArmy(myWar.saxonArmy, myWar.vikingArmy);
+myWar.attackArmy(myWar.saxonArmy, myWar.vikingArmy);
+myWar.attackArmy(myWar.saxonArmy, myWar.vikingArmy);
+myWar.attackArmy(myWar.saxonArmy, myWar.vikingArmy);
+myWar.attackArmy(myWar.saxonArmy, myWar.vikingArmy);
+myWar.attackArmy(myWar.vikingArmy, myWar.saxonArmy);
+myWar.attackArmy(myWar.saxonArmy, myWar.vikingArmy);
+myWar.attackArmy(myWar.vikingArmy, myWar.saxonArmy);
+
+console.log(myWar);
+console.log(myWar.showStatus());
